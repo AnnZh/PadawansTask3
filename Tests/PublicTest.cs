6641230c0c875c64e0f90edce70e9bef5f638c96
AnnZh <ann.zhurok@gmail.com>
2019-02-14 18:48:23 +0300

    Revert "Delete"
    This reverts commit d2eecb08774334dbadcdab4c7be1bae011e5dd9a.

--- Tests/PublicTest.cs
+++ Tests/PublicTest.cs
@@ -10,7 +10,7 @@ namespace PadawansTask3.Tests
         [TestCase(1, 1, ExpectedResult = 1)]
         [TestCase(8, 9, ExpectedResult = 1)]
         [TestCase(50, 250, ExpectedResult = 50)]
-        
+        [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
         public int GcdTests(int a, int b) => Gcd(a, b);
     }
 }
