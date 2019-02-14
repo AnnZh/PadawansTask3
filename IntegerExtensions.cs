392c474800b2bc0d77e0948188e1145b8c2f502b
AnnZh <ann.zhurok@gmail.com>
2019-02-14 18:38:45 +0300

    Done

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,56 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+            if (a > int.MaxValue || b > int.MaxValue)
+                throw new ArgumentException();
+            if (a < int.MinValue || b < int.MinValue)
+                throw new ArgumentException();
+
+            int m = Math.Abs(a);
+            int n = Math.Abs(b);
+           
             // put your code here
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            //на нули добавить всякую чухню
+
+            int d = 0;
+            int i = 0;
+            int j = 0;
+
+
+            if (n != m)
+            {
+                if ((n != 0) && (m != 0))
+                {
+                    if (m > n)
+                    {
+                        i = m;
+                    }
+                    else if (n > m)
+                    {
+                        i = n;
+                    }
+                        for (j = 1; j <= i / 2; j++)
+                        {
+                            if ((n % j == 0) && (m % j == 0))
+                                d = j;
+                        }
+                }
+                else
+                {
+                    if (n != 0)
+                        d = n;
+                    else d = m;                   
+                }
+            }
+            else
+            {
+                d = n;
+            }
+
+            return d;
         }
     }
 }
